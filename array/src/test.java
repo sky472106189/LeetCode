9106c0faccfac8dccfcbfe46c121681eee6d5e08
ywwup <ywwup@isoftstone.com>
2020-09-27 16:45:05 +0800

    数组「动态和」

---
+++ array/src/test.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+/**
+ * 给你一个日期，请你设计一个算法来判断它是对应一周中的哪一天。
+ *
+ * 输入为三个整数：day、month 和 year，分别表示日、月、年。
+ *
+ * 您返回的结果必须是这几个值中的一个 {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/day-of-the-week
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class test {
+    public static void main(String[] args) {
+
+    }
+
+    //1971.1.1-2100.12.31  1971.1.1是周五
+//    public String dayOfTheWeek(int day, int month, int year) {
+//        int sumDay = 365;
+//        if(year%4==0&&year%100!=0){
+//            sumDay+=1;
+//        }
+//
+//    }
+}
